c9f5fd535fb3f7839914d948dc11c2dbd3023b5b
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-02-13 18:58:40 -0800

    specify number of threads when running wsclean

--- pipeline/wsclean.jl
+++ pipeline/wsclean.jl
@@ -1,19 +1,19 @@
-function wsclean(name; weight="uniform", minuvw=0)
+function wsclean(name; weight="uniform", minuvw=0, j=1)
     if endswith(name, "ms")
         ms_name = name
         name = replace(name, ".ms", "")
     else
         ms_name = name*".ms"
     end
-    readstring(`wsclean -j 1 -size 2048 2048 -scale 0.0625 -weight $weight -minuv-l $minuvw -name $name $ms_name`)
+    readstring(`wsclean -j $j -size 2048 2048 -scale 0.0625 -weight $weight -minuv-l $minuvw -name $name $ms_name`)
     image_name = name*"-image.fits"
     dirty_name = name*"-dirty.fits"
     rm(dirty_name)
     mv(image_name, name*".fits", remove_destination=true)
 end
 
-function wsclean(input, output; weight="uniform", minuvw=0)
-    readstring(`wsclean -j 1 -size 2048 2048 -scale 0.0625 -weight $weight -minuv-l $minuvw -name $output $input`)
+function wsclean(input, output; weight="uniform", minuvw=0, j = 1)
+    readstring(`wsclean -j $j -size 2048 2048 -scale 0.0625 -weight $weight -minuv-l $minuvw -name $output $input`)
     rm(output*"-dirty.fits")
     mv(output*"-image.fits", output*".fits", remove_destination=true)
 end
