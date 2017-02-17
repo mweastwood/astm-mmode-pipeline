2e0fc6b268dd62127cc4a768786b2b51156e10ee
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-02-17 15:29:54 -0800

    minor refactoring of getalm to adjust for upstream changes

--- pipeline/getalm.jl
+++ pipeline/getalm.jl
@@ -1,17 +1,12 @@
-function getalm(spw, input="mmodes", output="alm", tolerance=0.05)
-    Lumberjack.info("Computing spherical harmonic coefficients for spectral window $spw")
+function getalm(spw, mmodes, mmode_flags, tolerance=0.01)
     dir = getdir(spw)
     transfermatrix = TransferMatrix(joinpath(dir, "transfermatrix"))
-    mmodes = MModes(joinpath(dir, input))
-    Lumberjack.info("Using transfer matrix $(transfermatrix.path)")
-    Lumberjack.info("Using m-modes $(mmodes.path)")
-    alm = _getalm(transfermatrix, mmodes, tolerance)
-    output = joinpath(dir, output*".jld")
-    Lumberjack.info("Saving the spherical harmonic coefficients to $output")
-    save(output, "alm", alm)
+    alm = _getalm(transfermatrix, mmodes, mmode_flags, tolerance)
+    save(joinpath(dir, "alm.jld"), "alm", alm)
+    alm
 end
 
-function _getalm(transfermatrix::TransferMatrix, mmodes, tolerance)
+function _getalm(transfermatrix::TransferMatrix, mmodes, mmode_flags, tolerance)
     lmax = transfermatrix.lmax
     mmax = transfermatrix.mmax
     alm = Alm(Complex128, lmax, mmax)
@@ -25,7 +20,8 @@ function _getalm(transfermatrix::TransferMatrix, mmodes, tolerance)
             input_channel  = RemoteChannel()
             output_channel = RemoteChannel()
             try
-                remotecall(getalm_remote_processing_loop, worker, input_channel, output_channel, transfermatrix, mmodes, tolerance)
+                remotecall(getalm_remote_processing_loop, worker, input_channel, output_channel,
+                           transfermatrix, mmodes, mmode_flags, tolerance)
                 Lumberjack.debug("Worker $worker has been started")
                 while true
                     m′ = nextm()
@@ -34,7 +30,7 @@ function _getalm(transfermatrix::TransferMatrix, mmodes, tolerance)
                     put!(input_channel, m′)
                     block = take!(output_channel)
                     for l = m′:lmax
-                        alm[l,m′] = block[l-m′+1]
+                        alm[l, m′] = block[l-m′+1]
                     end
                     increment_progress()
                 end
@@ -47,15 +43,29 @@ function _getalm(transfermatrix::TransferMatrix, mmodes, tolerance)
     alm
 end
 
-function getalm_remote_processing_loop(input, output, transfermatrix, mmodes, tolerance)
+function getalm_remote_processing_loop(input, output, transfermatrix, mmodes, mmode_flags, tolerance)
     BLAS.set_num_threads(16) # compensate for a bug in `addprocs`
     while true
-        m = take!(input)
-        A = transfermatrix[m,1]
-        b = mmodes[m,1]
-        BPJSpec.account_for_flags!(A, b)
-        x = tikhonov(A, b, tolerance)
-        put!(output, x)
+        try
+            m = take!(input)
+            A = transfermatrix[m, 1]
+            b = mmodes[m+1]
+            f = mmode_flags[m+1]
+            A = A[!f, :]
+            b = b[!f]
+            x = tikhonov(A, b, tolerance)
+            put!(output, x)
+        catch exception
+            if isa(exception, RemoteException) || isa(exception, InvalidStateException)
+                # If this is a remote worker, we will see a RemoteException when the channel is
+                # closed. However, if this is the master process (ie. we're running without any
+                # workers) then this will be an InvalidStateException. This is kind of messy...
+                break
+            else
+                println(exception)
+                rethrow(exception)
+            end
+        end
     end
 end
 
