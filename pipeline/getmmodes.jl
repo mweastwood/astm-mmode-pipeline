fef43912dd2904aab1d4efae830cd19dbdad4530
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-02-17 15:29:09 -0800

    refactor getmmodes to do less disk i/o and do the ft along the fast axis

--- pipeline/getmmodes.jl
+++ pipeline/getmmodes.jl
@@ -1,32 +1,60 @@
-function getmmodes(spw)
-    Lumberjack.info("Calculating m-modes for spectral window $spw")
-    dir = getdir(spw)
+function getmmodes(spw, data, flags)
+    mmax = 1000
+    Nbase, Ntime = size(data)
+    two(m) = ifelse(m != 0, 2, 1)
+    blocks = [zeros(Complex128, two(m)*Nbase) for m = 0:mmax]
+    fourier = do_fourier_transform(data)
+    pack_mmodes!(blocks, fourier, mmax)
+    block_flags = decide_on_baseline_flags(flags, mmax)
+    save(joinpath(getdir(spw), "mmodes.jld"), "blocks", blocks, "flags", block_flags, compress=true)
+    blocks, block_flags
+end
+
+function do_fourier_transform(data)
+    Nbase, Ntime = size(data)
     FFTW.set_num_threads(16)
+    transposed = permutedims(data, (2, 1)) # put time on the fast axis
+    planned_fft = plan_fft(transposed, 1)
+    fourier = planned_fft * transposed / Ntime
+    permutedims(fourier, (2, 1)) # undo the previous transpose
+end
 
-    path_to_visibilities = joinpath(dir, "visibilities.jld")
-    Lumberjack.info("Loading visibilities from $path_to_visibilities")
-    times, data, flags = load(path_to_visibilities, "times", "data", "flags")
+function pack_mmodes!(blocks, fourier, mmax)
+    Nbase, Ntime = size(fourier)
+    for m = 0:mmax
+        block = blocks[m+1]
+        if m == 0
+            for α = 1:Nbase
+                block[α] = fourier[α, 1]
+            end
+        else
+            for α = 1:Nbase
+                α1 = 2α - 1 # positive m
+                α2 = 2α - 0 # negative m
+                block[α1] =      fourier[α, m+1]
+                block[α2] = conj(fourier[α, Ntime+1-m])
+            end
+        end
+    end
+end
 
-    Lumberjack.info("Folding data in sidereal time")
-    combined = combine_days(spw, times, data, flags)
-
-    path_to_combined_visibilities = joinpath(dir, "visibilities")
-    Lumberjack.info("Saving combined visiblities to $path_to_combined_visibilities")
-    Nbase = size(combined, 1)
-    Ntime = size(combined, 2)
-    meta = getmeta(spw)
-    β = round(Int, middle(1:Nfreq(meta)))
-    ν = meta.channels[β:β]
-    origin = 0.0
-    visibilities = GriddedVisibilities(path_to_combined_visibilities, Nbase, Ntime, ν, origin)
-    visibilities[1] = combined
-    #visibilities.data[1][:] = combined
-    #visibilities.weights[1][:] = 1.0
-
-    path_to_mmodes = joinpath(dir, "mmodes")
-    Lumberjack.info("Computing m-modes and saving them to $path_to_mmodes")
-    mmodes = MModes(path_to_mmodes, visibilities, 1000)
-
-    nothing
+function decide_on_baseline_flags(flags, mmax)
+    # a baseline will be flagged if it has any sidereal times that are flagged
+    baseline_flags = squeeze(any(flags, 2), 2)
+    Nbase = length(baseline_flags)
+    block_flags = Vector{Bool}[]
+    for m = 0:mmax
+        if m == 0
+            push!(block_flags, baseline_flags)
+        else
+            f = fill(false, 2Nbase)
+            for α = 1:Nbase
+                f[2α - 1] = baseline_flags[α]
+                f[2α - 0] = baseline_flags[α]
+            end
+            push!(block_flags, f)
+        end
+    end
+    block_flags
 end
 
