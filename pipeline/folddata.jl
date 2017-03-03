887a297c9c600069ecb6de1c866ac6b022e893d5
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-03-03 13:12:30 -0800

    add a hook for adding some special case flags in problem scenarios

--- pipeline/folddata.jl
+++ pipeline/folddata.jl
@@ -1,9 +1,16 @@
+function fold(spw)
+    dir = getdir(spw)
+    data, flags = load(joinpath(dir, "peeled-visibilities.jld"), "data", "flags")
+    fold(spw, data, flags)
+end
+
 function fold(spw, data, flags)
     _, Nbase, Ntime = size(data)
     sidereal_day = 6628 # number of integrations in one sidereal day
     normalization = zeros(Int, Nbase, sidereal_day)
     output_data = zeros(Complex128, Nbase, sidereal_day)
     output_flags = trues(Nbase, sidereal_day)
+    flags = apply_special_case_flags(spw, flags)
     for idx = 1:Ntime, α = 1:Nbase
         if !flags[α, idx]
             jdx = mod1(idx, sidereal_day)
@@ -21,5 +28,13 @@ function fold(spw, data, flags)
     save(joinpath(getdir(spw), "folded-visibilities.jld"),
          "data", output_data, "flags", output_flags, compress=true)
     output_data, output_flags
+end
+
+function apply_special_case_flags(spw, flags)
+    myflags = copy(flags)
+    if spw == 18
+        myflags[:, 8590] = true # peeling failure associated with Vir A
+    end
+    myflags
 end
 
