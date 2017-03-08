36157ee13b9cc50772e41c3843e2c90b1d527d03
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-03-08 09:48:36 -0800

    remove some old files related to looking at the SVD of the residuals
    imagesvd.jl and reconstruct.jl
    
    I no longer believe that looking at the residuals is actually helpful
    for mitigating systematics in the maps.

--- pipeline/imagesvd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-OMP_NUM_THREAS=16 julia -e "using Pipeline; Pipeline.imagesvd($1, $2)"
-
--- pipeline/reconstruct.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-OMP_NUM_THREAS=16 julia -e "using Pipeline; Pipeline.reconstruct($1, \"$2\", \"$3\", \"$4\", $5)"
-
