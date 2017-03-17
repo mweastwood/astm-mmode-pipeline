de0328ad7393f38e8137941b75a447ff71bcde0b
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-03-17 09:31:16 -0700

    updates to the bash scripting
    This should now take care of getting the data through calibrating the
    data.

--- pipeline/bash/00-getdata.sh
+++ pipeline/bash/00-getdata.sh
@@ -5,7 +5,7 @@ set -eux
 JULIA=julia-0.5.0
 MACHINEFILE_MAX=workspace/machine-files/max-processes-each.machinefile
 
-cd `dirname $0/../..`
-$(JULIA) --machinefile $(MACHINEFILE_MAX) -e \
+cd `dirname $0`/../..
+$JULIA --machinefile $MACHINEFILE_MAX -e \
     "include(\"pipeline/Pipeline.jl\"); Pipeline.getdata($1, \"$2\")"
 
--- pipeline/bash/go.sh
+++ pipeline/bash/go.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
-./00-getdata.sh 18 rainy
-./01-flag.sh 18 raw-rainy-visibilities
+[ $1 -le 0 ] && ./00-getdata.sh 18 rainy
+[ $1 -le 1 ] && ./01-flag.sh 18 raw-rainy-visibilities
+[ $1 -le 2 ] && ./02-smooth 18 flagged-raw-rainy-visibilities
+[ $1 -le 3 ] && ./03-calibrate 18 smoothed-rainy-visibilities
 
