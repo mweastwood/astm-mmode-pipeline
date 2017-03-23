59b8f5046225decb5f9c6501d31ce217f03431b9
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-03-23 13:41:09 -0700

    minor bisect improvements
    * allow the user to specify the output directory
    
    * don't output the cas images to files named "cyg-range1.fits", etc.

--- pipeline/bisect.jl
+++ pipeline/bisect.jl
@@ -1,18 +1,18 @@
-function bisect_cyg(spw, target)
+function bisect_cyg(spw, target, output="/tmp")
     dir = getdir(spw)
     @time times, data, flags = load(joinpath(dir, target*".jld"), "times", "data", "flags")
     direction = Direction(dir"J2000", "19h59m28.35663s", "+40d44m02.0970s")
-    bisect(spw, times, data, flags, direction)
+    bisect(spw, times, data, flags, direction, "cyg", output)
 end
 
-function bisect_cas(spw, target)
+function bisect_cas(spw, target, output="/tmp")
     dir = getdir(spw)
     @time times, data, flags = load(joinpath(dir, target*".jld"), "times", "data", "flags")
     direction = Direction(dir"J2000", "23h23m24s", "58d48m54s")
-    bisect(spw, times, data, flags, direction)
+    bisect(spw, times, data, flags, direction, "cas", output)
 end
 
-function bisect(spw, times, data, flags, direction)
+function bisect(spw, times, data, flags, direction, name, output="/tmp")
     dir = getdir(spw)
     range = 1:length(times)
     println("Starting...")
@@ -21,12 +21,12 @@ function bisect(spw, times, data, flags, direction)
         range2 = range1[end]+1:range[end]
         @show range1 range2
         @time image_with_new_phase_center(spw, times, data, flags, range1, direction,
-                                          joinpath(dir, "tmp", "cyg-range1"))
+                                          joinpath(dir, "tmp", "$name-range1"))
         @time image_with_new_phase_center(spw, times, data, flags, range2, direction,
-                                          joinpath(dir, "tmp", "cyg-range2"))
+                                          joinpath(dir, "tmp", "$name-range2"))
 
-        run(`scp $(joinpath(dir, "tmp", "cyg-range1.fits")) mweastwood@smaug.caltech.edu:/tmp`)
-        run(`scp $(joinpath(dir, "tmp", "cyg-range2.fits")) mweastwood@smaug.caltech.edu:/tmp`)
+        run(`scp $(joinpath(dir, "tmp", "$name-range1.fits")) mweastwood@smaug.caltech.edu:$output`)
+        run(`scp $(joinpath(dir, "tmp", "$name-range2.fits")) mweastwood@smaug.caltech.edu:$output`)
 
         while true
             print("1 or 2? ")
