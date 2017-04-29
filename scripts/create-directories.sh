144953932d542cc7c0de44f0a0021f791a90073c
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-04-28 20:29:27 -0700

    fix create-directories.sh
    wasn't actually creating the temp directory on other machines

--- scripts/create-directories.sh
+++ scripts/create-directories.sh
@@ -8,7 +8,7 @@ cd `dirname $0`/..
 JULIA=julia-0.5.0
 MACHINEFILE=workspace/machine-files/one-process-each.machinefile
 
-$JULIA --machinefile $MACHINEFILE -e 'isdir("/dev/shm/mweastwood") || mkdir("/dev/shm/mweastwood")'
+$JULIA --machinefile $MACHINEFILE -e '@everywhere isdir("/dev/shm/mweastwood") || mkdir("/dev/shm/mweastwood")'
 $JULIA -e 'isdir("workspace") || mkdir("workspace")'
 $JULIA -e 'isdir("workspace/source-lists") || mkdir("workspace/source-lists")'
 $JULIA -e 'isdir("logs") || mkdir("logs")'
