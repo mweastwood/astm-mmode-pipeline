56ca9ba3515a53fe0eab32d53002f5ce5a22214a
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-04-30 03:10:40 -0700

    image after peeling always

--- pipeline/bash/12-peel.sh
+++ pipeline/bash/12-peel.sh
@@ -7,7 +7,8 @@ MACHINEFILE_MAX=../workspace/machine-files/max-processes-each.machinefile
 
 cd `dirname $0`/../..
 cd pipeline
-input="twice-rfi-subtracted-calibrated-$2-visibilities"
 $JULIA --machinefile $MACHINEFILE_MAX -e \
     "reload(\"Pipeline\"); @time Pipeline.Calibration.peel($1, \"$2\", \"rfi-subtracted-calibrated\")"
+$JULIA -e \
+    "reload(\"Interactive\"); @time Interactive.smeared_image_everything($1, \"$2\", \"peeled\")"
 
