9ba5e47c1c007634081d554af79a1bbe115c23e9
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-05-23 17:54:52 -0700

    begin moving the bash pipeline into a single file
    tons of small files was really annoying to manage and make changes to

--- pipeline/bash/go.sh
+++ pipeline/bash/go.sh
@@ -1,44 +1,98 @@
 #!/bin/bash
 
-set -eux
-cd `dirname $0`
+set -eu
+cd `dirname $0`/..
+
+JULIA=julia-0.5.0
+MACHINEFILE_ONE=../workspace/machine-files/one-process-each.machinefile
+
+#spws="4 6 8 10 12 14 16 18"
+#datasets="100hr rainy"
+
+spws=$3
+datasets=$4
 
 function title {
-    set +x
     len=`expr length $1`
     echo
     echo $1
     printf '%*s\n' $len | tr ' ' "="
-    set -x
+    date
 }
 
-spws=$3
-#spws="4 6 8 10 12 14 16 18"
+function quote {
+    echo "\"$1\""
+}
 
-datasets=$4
-#datasets="100hr rainy"
+function print_parameters {
+    echo "spw=$1, dataset=$2, target=$3"
+}
 
-for data in $datasets
+function addrfi {
+    title addrfi
+    local spw=$1
+    local dataset=`quote $2`
+    local target=`quote $3`
+    local rfi=`quote $4`
+    print_parameters $spw $dataset $target
+    $JULIA -e "using Pipeline; @time Pipeline.Calibration.addrfi($spw, $dataset, $target, $rfi)"
+}
+
+function fold {
+    title fold
+    local spw=$1
+    local dataset=`quote $2`
+    local target=`quote $3`
+    print_parameters $spw $dataset $target
+    $JULIA -e "using Pipeline; @time Pipeline.MModes.fold($spw, $dataset, $target)"
+}
+
+function getmmodes {
+    title getmmodes
+    local spw=$1
+    local dataset=`quote $2`
+    local target=`quote $3`
+    print_parameters $spw $dataset $target
+    $JULIA -e "using Pipeline; @time Pipeline.MModes.getmmodes($spw, $dataset, $target)"
+}
+
+function getalm {
+    title getalm
+    local spw=$1
+    local dataset=`quote $2`
+    local target=`quote $3`
+    print_parameters $spw $dataset $target
+    $JULIA --machinefile $MACHINEFILE_ONE -e \
+        "using Pipeline; @time Pipeline.MModes.getalm($spw, $dataset, $target)"
+}
+
+function makemap {
+    title makemap
+    local spw=$1
+    local dataset=`quote $2`
+    local target=`quote $3`
+    print_parameters $spw $dataset $target
+    $JULIA -e "using Pipeline; @time Pipeline.MModes.makemap($spw, $dataset, $target)"
+}
+
+for dataset in $datasets
 do
     for spw in $spws
     do
-        [ $1 -le 0 ] && [ $2 -ge 0 ] && title getdata && ./00-getdata.sh $spw $data
-        [ $1 -le 1 ] && [ $2 -ge 1 ] && title flag && ./01-flag.sh $spw $data
-        [ $1 -le 2 ] && [ $2 -ge 2 ] && title smooth && ./02-smooth.sh $spw $data
-        [ $1 -le 3 ] && [ $2 -ge 3 ] && title calibrate && ./03-calibrate.sh $spw $data
+        #[ $1 -le 0 ] && [ $2 -ge 0 ] && title getdata && ./00-getdata.sh $spw $dataset
+        #[ $1 -le 1 ] && [ $2 -ge 1 ] && title flag && ./01-flag.sh $spw $dataset
+        #[ $1 -le 2 ] && [ $2 -ge 2 ] && title smooth && ./02-smooth.sh $spw $dataset
+        #[ $1 -le 3 ] && [ $2 -ge 3 ] && title calibrate && ./03-calibrate.sh $spw $dataset
 
-        [ $1 -le 10 ] && [ $2 -ge 10 ] && title fitrfi && ./10-fitrfi.sh $spw $data
-        [ $1 -le 11 ] && [ $2 -ge 11 ] && title subrfi && ./11-subrfi.sh $spw $data
-        [ $1 -le 12 ] && [ $2 -ge 12 ] && title peel && ./12-peel.sh $spw $data
-        [ $1 -le 13 ] && [ $2 -ge 13 ] && title addrfi && ./13-addrfi.sh $spw $data
-        [ $1 -le 14 ] && [ $2 -ge 14 ] && title fitrfi-2 && ./14-fitrfi-2.sh $spw $data
-        [ $1 -le 15 ] && [ $2 -ge 15 ] && title subrfi-2 && ./15-subrfi-2.sh $spw $data
-        [ $1 -le 16 ] && [ $2 -ge 16 ] && title smeared-imaging && ./16-smeared-imaging.sh $spw $data
+        #[ $1 -le 10 ] && [ $2 -ge 10 ] && title fitrfi && ./10-fitrfi.sh $spw $dataset
+        #[ $1 -le 11 ] && [ $2 -ge 11 ] && title subrfi && ./11-subrfi.sh $spw $dataset
+        #[ $1 -le 12 ] && [ $2 -ge 12 ] && title peel && ./12-peel.sh $spw $dataset
+        [ $1 -le 13 ] && [ $2 -ge 13 ] && addrfi    $spw $dataset "peeled" "rfi-subtracted-calibrated"
 
-        [ $1 -le 20 ] && [ $2 -ge 20 ] && title fold && ./20-fold.sh $spw $data
-        [ $1 -le 21 ] && [ $2 -ge 21 ] && title getmmodes && ./21-getmmodes.sh $spw $data
-        [ $1 -le 22 ] && [ $2 -ge 22 ] && title getalm && ./22-getalm.sh $spw $data
-        [ $1 -le 23 ] && [ $2 -ge 23 ] && title makemap && ./23-makemap.sh $spw $data
+        [ $1 -le 20 ] && [ $2 -ge 20 ] && fold      $spw $dataset "rfi-restored-peeled"
+        [ $1 -le 21 ] && [ $2 -ge 21 ] && getmmodes $spw $dataset "folded-rfi-restored-peeled"
+        [ $1 -le 22 ] && [ $2 -ge 22 ] && getalm    $spw $dataset "mmodes-rfi-restored-peeled"
+        [ $1 -le 23 ] && [ $2 -ge 23 ] && makemap   $spw $dataset "alm-rfi-restored-peeled"
     done
 done
 
