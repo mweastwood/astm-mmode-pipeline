7abc6f40d1703e8ab7f6c3c2cd2387ae17e6c5eb
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-08-07 17:49:54 -0700

    get the psf at each and every ring

--- pipeline/Cleaning/getpsf.jl
+++ pipeline/Cleaning/getpsf.jl
@@ -49,21 +49,72 @@ function getpsf_peak(spw, dataset, nside=2048)
     dir = getdir(spw)
     observation_matrix, lmax, mmax = load(joinpath(dir, "observation-matrix-$dataset.jld"),
                                           "blocks", "lmax", "mmax")
-    N = 360
-    θ = linspace(0, π, N)
-    peaks = zeros(N)
-    prg = Progress(N)
-    for idx = 1:N
-        peaks[idx] = getpsf_peak(observation_matrix, θ[idx], 0, nside, lmax, mmax)
+    θ = Float64[]
+    pixels = Int[]
+    prg = Progress(nside2npix(nside)÷4)
+    for pixel = 1:4:nside2npix(nside)
+        myθ, myϕ = LibHealpix.pix2ang_ring(nside, pixel)
+        if !(myθ in θ)
+            push!(θ, myθ)
+            push!(pixels, pixel)
+        end
         next!(prg)
+    end
+
+    N = length(pixels)
+    peaks = zeros(N)
+    lck = ReentrantLock()
+    prg = Progress(N)
+
+    idx = 1
+    nextidx() = (myidx = idx; idx += 1; myidx)
+    output(peak, idx) = peaks[idx] = peak
+    increment() = (lock(lck); next!(prg); unlock(lck))
+    @sync for worker in workers()
+        @async begin
+            input_channel  = RemoteChannel()
+            output_channel = RemoteChannel()
+            try
+                remotecall(getpsf_worker_loop, worker, input_channel, output_channel,
+                           observation_matrix, nside, lmax, mmax)
+                while true
+                    myidx = nextidx()
+                    myidx ≤ N || break
+                    put!(input_channel, pixels[myidx])
+                    output(take!(output_channel), myidx)
+                    increment()
+                end
+            finally
+                close(input_channel)
+                close(output_channel)
+            end
+        end
     end
     save(joinpath(dir, "psf-peak-value-$dataset.jld"), "theta", θ, "peaks", peaks)
 end
 
-function getpsf_peak(observation_matrix, θ, ϕ, nside, lmax, mmax)
+function getpsf_worker_loop(input, output, observation_matrix, nside, lmax, mmax)
+    while true
+        try
+            pixel = take!(input)
+            put!(output, getpsf_peak(observation_matrix, pixel, nside, lmax, mmax))
+        catch exception
+            if isa(exception, RemoteException) || isa(exception, InvalidStateException)
+                break
+            else
+                @show m
+                run(`hostname`)
+                println(exception)
+                rethrow(exception)
+            end
+        end
+    end
+end
+
+function getpsf_peak(observation_matrix, pixel, nside, lmax, mmax)
+    θ, ϕ = LibHealpix.pix2ang_ring(nside, pixel)
     map = getpsf(observation_matrix, θ, ϕ, nside, lmax, mmax)
-    # TODO interpolate to the center of the PSF instead of just blindly taking the maximum
-    maximum(map.pixels)
+    map[pixel]
 end
 
 immutable PSFPeakValues
