7a337b405e7321e5e8e846d8d1d25d1874db2662
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-08-08 17:53:00 -0700

    save the psf on each ring of the healpix map

--- pipeline/Cleaning/getpsf.jl
+++ pipeline/Cleaning/getpsf.jl
@@ -1,33 +1,119 @@
-# Get the psf for the given spherical coordinates (θ, ϕ)
-
-function getpsf(spw, dataset, θ, ϕ, nside)
-    dir = getdir(spw)
-    observation_matrix, lmax, mmax = load(joinpath(dir, "observation-matrix-$dataset.jld"),
-                                          "blocks", "lmax", "mmax")
-    psf = getpsf(observation_matrix, θ, ϕ, 2048, lmax, mmax)
-    writehealpix(joinpath(dir, "psf.fits"), psf, replace=true)
+immutable PSF
+    pixels :: Vector{Int}
+    amplitudes :: Vector{Float64}
+    nside :: Int
+    lmax :: Int
+    mmax :: Int
 end
 
-function getpsf(observation_matrix, θ, ϕ, nside, lmax, mmax)
-    alm = getpsf_alm(observation_matrix, θ, ϕ, lmax, mmax)
-    alm2map(alm, nside)
+function getpsf(spw, dataset, nside=2048)
+    output_directory = joinpath(getdir(spw), "psf")
+    isdir(output_directory) || mkdir(output_directory)
+
+    path = joinpath(getdir(spw), "observation-matrix-$dataset.jld")
+    lmax, mmax, mrange = load(path, "lmax", "mmax", "mrange")
+    observation_matrix, cholesky_decomposition = load(path, "blocks", "cholesky")
+
+    pixels, amplitudes = _getpsf(observation_matrix, cholesky_decomposition,
+                                 nside, lmax, mmax, mrange, output_directory)
+
+    psf = PSF(pixels, amplitudes, nside, lmax, mmax)
+    save(joinpath(output_directory, "psf.jld"), "psf", psf)
+    psf
 end
 
-function getpsf_alm(observation_matrix, θ, ϕ, lmax, mmax)
-    input_alm = pointsource_alm(θ, ϕ, lmax, mmax)
-    output_alm = Alm(Complex128, lmax, mmax)
-    for m = 0:mmax
-        A = observation_matrix[m+1]
-        x = [input_alm[l, m] for l = m:lmax]
-        y = A*x
-        for l = m:lmax
-            output_alm[l, m] = y[l-m+1]
+function _getpsf(observation_matrix, cholesky_decomposition,
+                 nside, lmax, mmax, mrange, output_directory)
+    pixels = find_healpix_rings(nside)
+
+    N = length(pixels)
+    amplitudes = zeros(N)
+    lck = ReentrantLock()
+    prg = Progress(N)
+
+    idx = 1
+    nextidx() = (myidx = idx; idx += 1; myidx)
+    output(amplitude, idx) = amplitudes[idx] = amplitude
+    increment() = (lock(lck); next!(prg); unlock(lck))
+    @sync for worker in workers()
+        @async begin
+            input_channel  = RemoteChannel()
+            output_channel = RemoteChannel()
+            try
+                remotecall(getpsf_worker_loop, worker, input_channel, output_channel,
+                           observation_matrix, cholesky_decomposition,
+                           nside, lmax, mmax, mrange, output_directory)
+                while true
+                    myidx = nextidx()
+                    myidx ≤ N || break
+                    put!(input_channel, pixels[myidx])
+                    amplitude = take!(output_channel)
+                    output(amplitude, myidx)
+                    increment()
+                end
+            finally
+                close(input_channel)
+                close(output_channel)
+            end
         end
     end
+
+    pixels, amplitudes
+end
+
+function getpsf_worker_loop(input_channel, output_channel,
+                            observation_matrix, cholesky_decomposition,
+                            nside, lmax, mmax, mrange, output_directory)
+    while true
+        try
+            pixel = take!(input_channel)
+            peak = getpsf_and_save(observation_matrix, cholesky_decomposition,
+                                   nside, pixel, lmax, mmax, mrange, output_directory)
+            put!(output_channel, peak)
+        catch exception
+            if isa(exception, RemoteException) || isa(exception, InvalidStateException)
+                break
+            else
+                run(`hostname`)
+                println(exception)
+                rethrow(exception)
+            end
+        end
+    end
+end
+
+function getpsf_and_save(observation_matrix, cholesky_decomposition,
+                         nside, pixel, lmax, mmax, mrange, output_directory)
+    θ, ϕ = LibHealpix.pix2ang_ring(nside, pixel)
+    alm = getpsf(observation_matrix, cholesky_decomposition, θ, ϕ, lmax, mmax, mrange)
+    map = alm2map(alm, nside)
+    save(joinpath(output_directory, @sprintf("%08d.jld", pixel)), "alm", alm)
+    map[pixel]
+end
+
+function getpsf(observation_matrix, cholesky_decomposition, θ, ϕ, lmax, mmax, mrange)
+    input_alm = pointsource_alm(θ, ϕ, lmax, mmax)
+    output_alm = observe(observation_matrix, cholesky_decomposition, input_alm)
+    MModes.apply_wiener_filter!(output_alm, mrange)
     output_alm
 end
 
-import GSL
+function find_healpix_rings(nside)
+    npix = nside2npix(nside)
+    rings = Float64[]
+    pixels = Int[]
+    prg = Progress(npix÷4)
+    for pixel = 1:4:npix
+        θ, ϕ = LibHealpix.pix2ang_ring(nside, pixel)
+        if !(θ in rings)
+            push!(rings, θ)
+            push!(pixels, pixel)
+        end
+        next!(prg)
+    end
+    pixels
+end
+
 function pointsource_alm(θ, ϕ, lmax, mmax)
     cosθ = cos(θ)
     alm = Alm(Complex128, lmax, mmax)
@@ -41,99 +127,28 @@ function pointsource_alm(θ, ϕ, lmax, mmax)
     alm
 end
 
-# We want to be able to translate pixel values in the map to flux units. So we will compute the PSF
-# at a range of declinations and read off the peak pixel value. We will then interpolate this
-# function while cleaning.
-
-function getpsf_peak(spw, dataset, nside=2048)
-    dir = getdir(spw)
-    observation_matrix, lmax, mmax = load(joinpath(dir, "observation-matrix-$dataset.jld"),
-                                          "blocks", "lmax", "mmax")
-    θ = Float64[]
-    pixels = Int[]
-    prg = Progress(nside2npix(nside)÷4)
-    for pixel = 1:4:nside2npix(nside)
-        myθ, myϕ = LibHealpix.pix2ang_ring(nside, pixel)
-        if !(myθ in θ)
-            push!(θ, myθ)
-            push!(pixels, pixel)
-        end
-        next!(prg)
-    end
-
-    N = length(pixels)
-    peaks = zeros(N)
-    lck = ReentrantLock()
-    prg = Progress(N)
-
-    idx = 1
-    nextidx() = (myidx = idx; idx += 1; myidx)
-    output(peak, idx) = peaks[idx] = peak
-    increment() = (lock(lck); next!(prg); unlock(lck))
-    @sync for worker in workers()
-        @async begin
-            input_channel  = RemoteChannel()
-            output_channel = RemoteChannel()
-            try
-                remotecall(getpsf_worker_loop, worker, input_channel, output_channel,
-                           observation_matrix, nside, lmax, mmax)
-                while true
-                    myidx = nextidx()
-                    myidx ≤ N || break
-                    put!(input_channel, pixels[myidx])
-                    output(take!(output_channel), myidx)
-                    increment()
-                end
-            finally
-                close(input_channel)
-                close(output_channel)
-            end
+function observe(observation_matrix, cholesky_decomposition, input_alm)
+    output_alm = Alm(Complex128, lmax(input_alm), mmax(input_alm))
+    for m = 0:mmax(input_alm)
+        x = [input_alm[l, m] for l = m:lmax(input_alm)]
+        y = observe_block(observation_matrix[m+1], cholesky_decomposition[m+1], x)
+        for l = m:lmax(input_alm)
+            output_alm[l, m] = y[l-m+1]
         end
     end
-    save(joinpath(dir, "psf-peak-value-$dataset.jld"), "theta", θ, "peaks", peaks)
+    output_alm
 end
 
-function getpsf_worker_loop(input, output, observation_matrix, nside, lmax, mmax)
-    while true
-        try
-            pixel = take!(input)
-            put!(output, getpsf_peak(observation_matrix, pixel, nside, lmax, mmax))
-        catch exception
-            if isa(exception, RemoteException) || isa(exception, InvalidStateException)
-                break
-            else
-                @show m
-                run(`hostname`)
-                println(exception)
-                rethrow(exception)
-            end
-        end
-    end
+function observe_block(observation_matrix, cholesky_decomposition, input_alm)
+    output_alm = similar(input_alm)
+    A_mul_B!(output_alm, observation_matrix, input_alm)
+    Ac_ldiv_B!(cholesky_decomposition, output_alm)
+    A_ldiv_B!(cholesky_decomposition, output_alm)
+    output_alm
 end
 
-function getpsf_peak(observation_matrix, pixel, nside, lmax, mmax)
-    θ, ϕ = LibHealpix.pix2ang_ring(nside, pixel)
-    map = getpsf(observation_matrix, θ, ϕ, nside, lmax, mmax)
-    map[pixel]
-end
-
-immutable PSFPeakValues
-    θ :: Vector{Float64} # angle from north in radians
-    values :: Vector{Float64}
-end
-
-function loadpsf_peak(spw::Integer, dataset)
-    dir = getdir(spw)
-    θ, values = load(joinpath(dir, "psf-peak-value-$dataset.jld"), "theta", "peaks")
-    PSFPeakValues(θ, values)
-end
-
-function loadpsf_peak(spws::AbstractVector, dataset)
-    PSFPeakValues[loadpsf_peak(spw, dataset) for spw in spws]
-end
-
-function getpeak(psfpeaks::PSFPeakValues, θ)
-    idx = searchsortedlast(psfpeaks.θ, θ)
-    psfpeaks.values[idx]
-end
+#function getpeak(psfpeaks::PSFPeakValues, θ)
+#    idx = searchsortedlast(psfpeaks.θ, θ)
+#    psfpeaks.values[idx]
+#end
 
