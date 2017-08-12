f5dd901d01d28d3567adf99dce8e82905b84a688
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-08-12 14:07:45 -0700

    wip on cleaning

--- pipeline/Cleaning/Cleaning.jl
+++ pipeline/Cleaning/Cleaning.jl
@@ -7,205 +7,21 @@ using LibHealpix
 using TTCal
 using BPJSpec
 using NLopt
+import GSL
 using ..Common
+import ..MModes
 
-include("fix-flux-scale.jl")
-include("register.jl")
+#include("fix-flux-scale.jl")
+#include("register.jl")
 
 include("getpsf.jl")
-#include("clean-worker.jl")
 include("clean.jl")
-include("postage-stamp.jl")
 
 #include("clean-worker.jl")
+#include("clean.jl")
+#include("postage-stamp.jl")
 
-#function clean(spw, dataset="rainy")
-#    dir = getdir(spw)
-#    map = readhealpix(joinpath(dir, "map-rfi-subtracted-peeled-$dataset-itrf.fits"))
-#    alm = load(joinpath(dir, "alm-rfi-subtracted-peeled-$dataset.jld"), "alm")
-#    regions = find_sources_in_the_map(spw, dataset)
-#    @show length(regions)
-#    cleaning_regions = construct_cleaning_regions(map, regions)
-#    @show length(cleaning_regions)
-#    cleaning_regions = discard_nearby_cleaning_regions(map, cleaning_regions)
-#    @show length(cleaning_regions)
-#
-#
-#    #meta = getmeta(spw, dataset)
-#    #frame = TTCal.reference_frame(meta)
-#    #for region in cleaning_regions
-#    #    j2000 = measure(frame, region.center, dir"J2000")
-#    #    image = GetPSF.extract_image(map, region.center)
-#
-#    #    @show j2000
-#
-#    #    figure(1); clf()
-#    #    imshow(image, interpolation="nearest")
-#    #    gca()[:set_aspect]("equal")
-#    #    grid("on")
-#    #    colorbar()
-#
-#    #    print("Continue? ")
-#    #    inp = chomp(readline())
-#    #    inp == "q" && break
-#    #end
-#
-#
-#
-#
-#
-#    map = _clean(spw, dataset, map, lmax(alm), mmax(alm), cleaning_regions)
-#    writehealpix(joinpath(dir, "tmp", "cleaned.fits"), map, replace=true)
-#    map
-#end
-#
-#function _clean(spw, dataset, map, lmax, mmax, cleaning_regions)
-#    println("===")
-#    @time @sync for worker in workers()
-#        @async begin
-#            remotecall_fetch(CleanWorker.read_observation_matrix, worker, spw, dataset)
-#            remotecall_fetch(CleanWorker.set_lmax_mmax, worker, lmax, mmax)
-#        end
-#    end
-#
-#    N = length(cleaning_regions)
-#    completed = fill(false, N)
-#    for iteration = 1:10
-#        @show iteration
-#        incomplete_regions = @view cleaning_regions[!completed]
-#        completed_view = @view completed[!completed]
-#        indices = pick_regions(map, incomplete_regions, 100)
-#
-#        function nextidx()
-#            if length(indices) == 0
-#                return 0
-#            else
-#                return pop!(indices)
-#            end
-#        end
-#
-#        prg = Progress(length(indices), "Progress: ")
-#        lck = ReentrantLock()
-#        increment_progress() = (lock(lck); next!(prg); unlock(lck))
-#
-#        list_of_indices = Int[]
-#        list_of_fluxes = Float64[]
-#        list_of_centroids = Direction[]
-#        list_of_psfs = HealpixMap[]
-#        function update_lists(idx, flux, centroid, psf)
-#            push!(list_of_indices, idx)
-#            push!(list_of_fluxes, flux)
-#            push!(list_of_centroids, centroid)
-#            push!(list_of_psfs, psf)
-#        end
-#
-#        @time @sync for worker in workers()
-#            @async remotecall_fetch(CleanWorker.set_map, worker, map)
-#        end
-#
-#        @time @sync for worker in workers()
-#            @async while true
-#                idx = nextidx()
-#                idx == 0 && break
-#                region = cleaning_regions[idx]
-#                flux, centroid, psf = remotecall_fetch(CleanWorker.clean_major_iteration, worker, region)
-#                update_lists(idx, flux, centroid, psf)
-#                increment_progress()
-#            end
-#        end
-#
-#        λ = 0.15
-#        for (idx, flux, centroid, psf) in zip(list_of_indices, list_of_fluxes,
-#                                              list_of_centroids, list_of_psfs)
-#            map -= λ*psf
-#            if abs(flux) < 0.1
-#                completed_view[idx] = true
-#            end
-#        end
-#    end
-#    map
-#end
-#
-#immutable CleaningRegion
-#    center :: Direction
-#    aperture :: Set{Int}
-#    annulus  :: Set{Int}
-#end
-#
-#function construct_cleaning_regions(map, regions)
-#    N = length(map)
-#    rhat = zeros(3, N)
-#    for pixel = 1:N
-#        rhat[:, pixel] = LibHealpix.pix2vec_ring(nside(map), pixel)
-#    end
-#
-#    output = CleaningRegion[]
-#    for region in regions
-#        center = CleanWorker.get_region_centroid(map, region)
-#        aperture = Set{Int}()
-#        annulus  = Set{Int}()
-#        for pixel = 1:N
-#            dotproduct = (rhat[1, pixel]*center.x
-#                          + rhat[2, pixel]*center.y
-#                          + rhat[3, pixel]*center.z)
-#            # I'm seeing cases where the dot product is slightly out of the domain of arccos.
-#            # Eg. +1.0000000000000002, -1.0000000000000002
-#            dotproduct = min(dotproduct, +1.0)
-#            dotproduct = max(dotproduct, -1.0)
-#            θ = acosd(dotproduct)
-#            if θ < 0.25
-#                push!(aperture, pixel)
-#            elseif 3 < θ < 5
-#                push!(annulus, pixel)
-#            end
-#        end
-#        push!(output, CleaningRegion(center, aperture, annulus))
-#    end
-#    output
-#end
-#
-#function discard_nearby_cleaning_regions(map, regions)
-#    # If two regions are too close to each other, we should discard the fainter one. This will
-#    # prevent us from erroneously picking out sidelobes before we have had an opportunity to
-#    # deconvolve them.
-#    done = false
-#    while !done
-#        done = true
-#        for idx = 1:length(regions), jdx = idx+1:length(regions)
-#            r1 = regions[idx]
-#            r2 = regions[jdx]
-#            a = r1.center
-#            b = r2.center
-#            θ = acosd(a.x*b.x + a.y*b.y + a.z*b.z)
-#            if θ < 2
-#                @show a b
-#                background1 = CleanWorker.get_region_median(map, r1.annulus)
-#                background2 = CleanWorker.get_region_median(map, r2.annulus)
-#                amplitude1 = maximum(map[pixel] - background1 for pixel in r1.aperture)
-#                amplitude2 = maximum(map[pixel] - background2 for pixel in r2.aperture)
-#                if amplitude1 ≥ amplitude2
-#                    deleteat!(regions, jdx)
-#                else
-#                    deleteat!(regions, idx)
-#                end
-#                done = false
-#                break
-#            end
-#        end
-#    end
-#    regions
-#end
-#
-#function pick_regions(map, cleaning_regions, N)
-#    N = min(N, length(cleaning_regions))
-#    residuals = zeros(length(cleaning_regions))
-#    for (idx, region) in enumerate(cleaning_regions)
-#        background = CleanWorker.get_region_median(map, region.annulus)
-#        residuals[idx] = maximum(map[pixel]-background for pixel in region.aperture)
-#    end
-#    perm = sortperm(residuals, rev=true)
-#    perm[1:N]
-#end
+#include("clean-worker.jl")
 
 end
 
--- pipeline/Cleaning/clean.jl
+++ pipeline/Cleaning/clean.jl
@@ -1,58 +1,81 @@
-function clean(spw, dataset, target="map-wiener-filtered")
-    println("Cleaning")
-    dir = getdir(spw)
-    matrix = load(joinpath(dir, "observation-matrix-$dataset.jld"), "blocks")
-    psf = loadpsf_peak(spw, dataset)
-    map = readhealpix(joinpath(dir, "$target-$dataset-2048-itrf.fits"))
-    tolerance = load(joinpath(dir, "$(replace(target, "map", "alm"))-$dataset.jld"), "tolerance")
-    cholesky = cholesky_decomposition(matrix, tolerance)
-    x, y, z = unit_vectors(nside(map))
-    mask = create_mask(spw, dataset, x, y, z)
-    clean(spw, dataset, target, matrix, cholesky, psf, map, mask, x, y, z)
+function clean(spw, dataset, target)
+    output_directory = joinpath(getdir(spw), "cleaning")
+    isdir(output_directory) || mkdir(output_directory)
+
+    psf = load(joinpath(getdir(spw), "psf", "psf.jld"), "psf")
+    residual_alm, mrange =
+        load(joinpath(getdir(spw), "$target-$dataset.jld"), "alm", "mrange")
+    observation_matrix_path = joinpath(getdir(spw), "observation-matrix-$dataset.jld")
+    observation_matrix, cholesky_decomposition =
+        load(observation_matrix_path, "blocks", "cholesky")
+    workload = distribute_workload(observation_matrix, cholesky_decomposition)
+
+    nside = 2048
+    x, y, z = unit_vectors(nside)
+    clean_mask = create_mask(spw, dataset, x, y, z)
+    clean_components = HealpixMap(Float64, nside)
+
+    maxiter = 2048
+    Npixels = 256
+
+    clean(spw, dataset, target, psf, residual_alm,
+          workload, clean_mask, clean_components, x, y, z,
+          maxiter, mrange, Npixels, output_directory)
 end
 
-function clean(spw, dataset, target, matrix, cholesky, psf, map, mask, x, y, z)
-    #maxiter = 1024
-    maxiter = 50
-    components = HealpixMap(Float64, nside(map))
+function clean(spw, dataset, target, psf, residual_alm,
+               workload, clean_mask, clean_components, x, y, z,
+               maxiter, mrange, Npixels, output_directory)
+
+    residual_map = alm2map(residual_alm, nside(clean_components))
     for iter = 1:maxiter
         println("================")
         @printf("Iteration #%05d\n", iter)
-        @time map, components = clean_iteration(matrix, cholesky, psf, map, components, mask, x, y, z)
-        #if mod(iter, 128) == 0
-        if mod(iter, 1) == 0
+        println("time = ", now())
+        println("stddev = ", std(residual_map.pixels[clean_mask]))
+
+        @time residual_map = clean_iteration!(psf, residual_alm, residual_map,
+                                              workload, clean_mask, clean_components,
+                                              x, y, z, mrange, Npixels)
+
+        if mod(iter, 128) == 0
             println("...writing maps...")
             dir = getdir(spw)
             iterstr = @sprintf("%05d", iter)
-            #filename = "cleaned-$target-$dataset-$iterstr.fits"
-            filename = "test-cleaned-$target-$dataset-$iterstr.fits"
-            #writehealpix(joinpath(dir, "tmp", filename), map, replace=true)
-            writehealpix(joinpath(dir, "tmp", filename), alm2map(map2alm(map, 1000, 1000), 512), replace=true)
-            ##filename = "clean-components-$target-$dataset-$iterstr.fits"
-            #filename = "test-clean-components-$target-$dataset-$iterstr.fits"
-            #writehealpix(joinpath(dir, "tmp", filename), components, replace=true)
+            filename = "residual-map-$iterstr.fits"
+            writehealpix(joinpath(output_directory, filename), residual_map, replace=true)
+            filename = "clean-components-$iterstr.fits"
+            writehealpix(joinpath(output_directory, filename), clean_components, replace=true)
         end
     end
+
+    save(joinpath(output_directory, "final.jld"),
+         "residual_alm", residual_alm.alm,
+         "residual_map", residual_map.pixels,
+         "clean_components", clean_components.pixels)
 end
 
-function clean_iteration(matrix, cholesky, psf, map, components, mask, x, y, z)
-    #Npixels = 256
-    Npixels = 1
+function clean_iteration!(psf, residual_alm, residual_map,
+                          workload, clean_mask, clean_components,
+                          x, y, z, mrange, Npixels)
+
     println("* selecting pixels")
-    @time pixels = select_pixels(map, mask, x, y, z, Npixels)
-    println("* spherical harmonics")
-    @time model_alm = spherical_harmonics(map, components, psf, pixels)
-    println("* observing")
-    @time model_alm = observe(matrix, cholesky, model_alm)
-    println("* spherical harmonic transform")
-    @time model_map = alm2map(model_alm, nside(map))
-    println("* subtracting model")
-    @time map = map - 0.15*model_map
-    map, components
-end
+    @time pixels = select_pixels(residual_map, clean_mask, x, y, z, Npixels)
 
-function cholesky_decomposition(matrix, tolerance)
-    [chol(block + tolerance*I) for block in matrix]
+    println("* computing spherical harmonics")
+    @time model_alm = compute_spherical_harmonics!(residual_alm, residual_map,
+                                                   clean_components, psf, pixels)
+
+    println("* corrupting spherical harmonics")
+    @time corrupt_spherical_harmonics!(model_alm, workload, mrange)
+
+    println("* removing clean components")
+    @time residual_alm.alm[:] -= model_alm.alm
+
+    println("* creating new residual map")
+    @time residual_map = alm2map(residual_alm, nside(residual_map))
+
+    residual_map
 end
 
 function create_mask(spw, dataset, x, y, z)
@@ -60,26 +83,23 @@ function create_mask(spw, dataset, x, y, z)
     mask = Int[]
     meta = getmeta(spw, dataset)
     frame = TTCal.reference_frame(meta)
-    #per_a = measure(frame, Direction(dir"J2000", "03h19m48.16010s", "+41d30m42.1031s"), dir"ITRF")
-    _3c_134 = measure(frame, Direction(dir"J2000", "05h04m42.0s", "+38d06m02s"), dir"ITRF")
+    #_per_a = measure(frame, Direction(dir"J2000", "03h19m48.16010s", "+41d30m42.1031s"), dir"ITRF")
+    #_3c134 = measure(frame, Direction(dir"J2000", "05h04m42.0s", "+38d06m02s"), dir"ITRF")
     for pixel = 1:N
-        #dotproduct = x[pixel]*per_a.x + y[pixel]*per_a.y + z[pixel]*per_a.z
-        dotproduct = x[pixel]*_3c_134.x + y[pixel]*_3c_134.y + z[pixel]*_3c_134.z
-        distance = acosd(clamp(dotproduct, -1, 1))
-        if distance < 0.2
+        declination = acosd(z[pixel])
+        #dotproduct = x[pixel]*_3c134.x + y[pixel]*_3c134.y + z[pixel]*_3c134.z
+        #distance = acosd(clamp(dotproduct, -1, 1))
+        if declination > -30 #&& distance < 0.5
             push!(mask, pixel)
         end
     end
-    @show length(mask)
     mask
 end
 
-function select_pixels(map, mask, x, y, z, N)
-    #sorted_pixels = sortperm(abs(map.pixels))
-    #sorted_pixels = sortperm(map.pixels)
-    sorted_pixels = mask[sortperm(map.pixels[mask])]
+function select_pixels(residual_map, clean_mask, x, y, z, Npixels)
+    sorted_pixels = clean_mask[sortperm(residual_map.pixels[clean_mask])]
     selected_pixels = Int[]
-    while length(selected_pixels) < N
+    while length(selected_pixels) < Npixels
         @label top
         # take the pixel with the largest absolute value
         pixel = pop!(sorted_pixels)
@@ -95,66 +115,86 @@ function select_pixels(map, mask, x, y, z, N)
         end
         push!(selected_pixels, pixel)
     end
-    selected_pixels
+    sort(selected_pixels)
 end
 
-function spherical_harmonics(map, components, psf, pixels)
-    lmax = mmax = 1000
-    alm  = Alm(Complex128, lmax, mmax)
-    function output(myalm, θ, ϕ, pixel)
-        scale = map[pixel] / getpeak(psf, θ)
-        components[pixel] += scale
-        alm.alm[:] += scale * myalm.alm
+function compute_spherical_harmonics!(residual_alm, residual_map,
+                                      clean_components, psf, pixels)
+    L = lmax(residual_alm)
+    M = mmax(residual_alm)
+    N = nside(residual_map)
+
+    model_alm = Alm(Complex128, L, M)
+    function add_component(component_alm, pixel)
+        ring = searchsortedlast(psf.pixels, pixel)
+        scale = 0.15*residual_map[pixel]/psf.amplitudes[ring]
+        clean_components[pixel] += scale
+        model_alm.alm[:] += scale*component_alm.alm
     end
-    not_done() = length(pixels) > 0
+
     next_pixel() = pop!(pixels)
-    angles(pixel) = LibHealpix.pix2ang_ring(nside(map), pixel)
+    done() = length(pixels) == 0
     @sync for worker in workers()
-        @async while not_done()
-            mypixel = next_pixel()
-            θ, ϕ = angles(mypixel)
-            myalm = remotecall_fetch(pointsource_alm, worker, θ, ϕ, lmax, mmax)
-            output(myalm, θ, ϕ, mypixel)
+        @async while !done()
+            pixel′ = next_pixel()
+            θ, ϕ = LibHealpix.pix2ang_ring(N, pixel′)
+            component_alm = remotecall_fetch(pointsource_alm, worker, θ, ϕ, L, M)
+            add_component(component_alm, pixel′)
         end
     end
-    alm
+
+    model_alm
 end
 
-function observe(observation_matrix, cholesky, input_alm)
-    output_alm = Alm(Complex128, lmax(input_alm), mmax(input_alm))
-    for m = 0:mmax(input_alm)
-        #A = observation_matrix[m+1]
-        BB = observation_matrix[m+1]
-        U  = cholesky[m+1]
-        L  = U'
+module CleanWorker
+    const M = Matrix{Complex128}
+    const U = UpperTriangular{Complex128, M}
+    const observation_matrix_blocks = Dict{Int, M}()
+    const cholesky_decomposition_blocks = Dict{Int, U}()
+end
 
-        x = [input_alm[l, m] for l = m:lmax(input_alm)]
-        #y = A*x
-        y = U\(L\(BB*x))
-        for l = m:lmax(input_alm)
-            output_alm[l, m] = y[l-m+1]
+function transfer_blocks(m, observation_matrix_block, cholesky_decomposition_block)
+    CleanWorker.observation_matrix_blocks[m] = observation_matrix_block
+    CleanWorker.cholesky_decomposition_blocks[m] = cholesky_decomposition_block
+end
+
+function distribute_workload(observation_matrix, cholesky_decomposition)
+    mmax = length(observation_matrix)-1
+    workload = Dict{Int, Int}()
+    @sync for (worker, m) in zip(cycle(workers()), 0:mmax)
+        workload[m] = worker
+        @async remotecall_wait(transfer_blocks, worker, m,
+                               observation_matrix[m+1],
+                               cholesky_decomposition[m+1])
+    end
+    empty!(observation_matrix)
+    empty!(cholesky_decomposition)
+    workload
+end
+
+function corrupt_spherical_harmonics!(model_alm, workload, mrange)
+    function output(corrupted_alm, m)
+        for l = m:lmax(model_alm)
+            model_alm[l, m] = corrupted_alm[l-m+1]
         end
     end
-    output_alm
+    @sync for m = 0:mmax(model_alm)
+        @async begin
+            worker = workload[m]
+            input_alm = [model_alm[l, m] for l = m:lmax(model_alm)]
+            corrupted_alm = remotecall_fetch(corrupt_spherical_harmonics_block,
+                                             worker, input_alm, m)
+            output(corrupted_alm, m)
+        end
+    end
+    MModes.apply_wiener_filter!(model_alm, mrange)
 end
 
-#function spherical_harmonic_transforms(pool, alms, nside)
-#    sht(alm) = alm2map(alm, nside)
-#    workers = pool.spherical_harmonic_transform_workers
-#    futures = [remotecall(sht, worker, alm) for (worker, alm) in zip(workers, alms)]
-#    HealpixMap[fetch(future) for future in futures]
-#end
-#
-#function subtract_model(maps, models, λ)
-#    [map - λ*model for (map, model) in zip(maps, models)]
-#end
-#
-#function readmap(spw, dataset, target)
-#    dir = getdir(spw)
-#    readhealpix(joinpath(dir, "$target-$dataset-2048-itrf.fits"))
-#end
-#
-#readmaps(spws, dataset, target) = HealpixMap[readmap(spw, dataset, target) for spw in spws]
+function corrupt_spherical_harmonics_block(input_alm, m)
+    observe_block(CleanWorker.observation_matrix_blocks[m],
+                  CleanWorker.cholesky_decomposition_blocks[m],
+                  input_alm)
+end
 
 function unit_vectors(nside)
     npix = nside2npix(nside)
@@ -168,5 +208,14 @@ function unit_vectors(nside)
         z[pix] = vec[3]
     end
     x, y, z
+end
+
+function rotate_alm!(alm, dϕ)
+    for m = 0:mmax(alm)
+        cismdϕ = cis(-m*dϕ)
+        for l = m:lmax(alm)
+            alm[l, m] *= cismdϕ
+        end
+    end
 end
 
