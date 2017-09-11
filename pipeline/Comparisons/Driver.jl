69f5cc0c0ee51f00702ff46a1445999480cb2179
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-09-11 10:55:31 -0700

    spectral index fits for the lwa maps

--- pipeline/Comparisons/Driver.jl
+++ pipeline/Comparisons/Driver.jl
@@ -6,6 +6,7 @@ using PyPlot
 using JLD
 using LibHealpix
 using CasaCore.Measures
+using TTCal
 using BPJSpec
 using NLopt
 using LsqFit
@@ -148,71 +149,139 @@ function compare_with_guzman()
 end
 
 function internal_comparison()
-    #ν = Float64[]
-    #maps = HealpixMap[]
-    #for spw = 4:2:18
-    #    @show spw
-    #    myν = Pipeline.Common.getfreq(spw)
-    #    dir = Pipeline.Common.getdir(spw)
-    #    map = readhealpix(joinpath(dir, "map-restored-registered-rainy-itrf.fits"))
-    #    map = map * (BPJSpec.Jy * (BPJSpec.c/myν)^2 / (2*BPJSpec.k))
-    #    #map = Pipeline.MModes.rotate_to_galactic(spw, "rainy", map)
-    #    push!(maps, map)
-    #    push!(ν, myν)
-    #end
-    #save("checkpoint.jld", "maps", getfield.(maps, 1), "frequencies", ν)
-    @time pixels, ν = load("checkpoint.jld", "maps", "frequencies")
-    @time maps = [HealpixMap(pixels[idx]) for idx = 1:length(pixels)]
+    filename = "map-restored-registered-rainy-itrf.fits"
+    filename_odd  = "map-odd-restored-registered-rainy-itrf.fits"
+    filename_even = "map-even-restored-registered-rainy-itrf.fits"
 
-    @time spectral_index = fit_spectral_index(ν, maps)
+    trials = [(filename, filename, filename),
+              (filename, filename, filename_odd ),
+              (filename, filename, filename_even),
+              (filename, filename_odd,  filename),
+              (filename, filename_even, filename),
+              (filename_odd, filename,  filename),
+              (filename_even, filename, filename)]
 
+    line_slope     = Vector{Float64}[]
+    plane_slope_1  = Vector{Float64}[]
+    plane_slope_2  = Vector{Float64}[]
+    line_residual  = Vector{Float64}[]
+    plane_residual = Vector{Float64}[]
+
+    futures = [remotecall(fit_plane, idx+1, trials[idx]...) for idx = 1:length(trials)]
+    for future in futures
+        tmp = fetch(future)
+        push!(line_slope,     tmp[1])
+        push!(plane_slope_1,  tmp[2])
+        push!(plane_slope_2,  tmp[3])
+        push!(line_residual,  tmp[4])
+        push!(plane_residual, tmp[5])
+    end
+
+    save("internal-spectral-index.jld",
+         "line_slope", line_slope, "plane_slope_1", plane_slope_1, "plane_slope_2", plane_slope_2,
+         "line_residual", line_residual, "plane_residual", plane_residual)
 end
 
-function fit_spectral_index(ν, maps)
-    Npix = length(maps[1])
-    spectral_slope = zeros(Nmap-1, Npix)
-    spectral_slope_error = zeros(Nmap-1, Npix)
-    for idx = 1:1
-        θ, ϕ = LibHealpix.pix2ang_ring(nside(maps[1]), idx)
-        dec = rad2deg(π/2-θ)
+function fit_plane(filename1, filename2, filename3; width=5)
+    println("Preparing 1...")
+    ν1 = Pipeline.Common.getfreq(4)
+    @time map1 = readhealpix(joinpath(Pipeline.Common.getdir(4), filename1))
+    @time map1 = map1 * (BPJSpec.Jy * (BPJSpec.c/ν1)^2 / (2*BPJSpec.k))
+    @time map1 = Pipeline.MModes.rotate_to_galactic(4, "rainy", map1)
+    @time map1 = degrade(map1, 512)
+
+    println("Preparing 2...")
+    ν2 = Pipeline.Common.getfreq(10)
+    @time map2 = readhealpix(joinpath(Pipeline.Common.getdir(10), filename2))
+    @time map2 = map2 * (BPJSpec.Jy * (BPJSpec.c/ν2)^2 / (2*BPJSpec.k))
+    @time map2 = Pipeline.MModes.rotate_to_galactic(4, "rainy", map2)
+    @time map2 = degrade(map2, 512)
+
+    println("Preparing 3...")
+    ν3 = Pipeline.Common.getfreq(18)
+    @time map3 = readhealpix(joinpath(Pipeline.Common.getdir(18), filename1))
+    @time map3 = map3 * (BPJSpec.Jy * (BPJSpec.c/ν3)^2 / (2*BPJSpec.k))
+    @time map2 = Pipeline.MModes.rotate_to_galactic(4, "rainy", map2)
+    @time map3 = degrade(map3, 512)
+
+    println("Fitting...")
+    @time _fit_plane(map1, map2, map3, width)
+end
+
+function _fit_plane(map1, map2, map3, width)
+    output_nside = 128
+    output_npix  = nside2npix(output_nside)
+    line_slope     = zeros(output_npix) # slope of a linear fit between map1 and map3
+    plane_slope_1  = zeros(output_npix) # slope of a planar fit between map3 and map1
+    plane_slope_2  = zeros(output_npix) # slope of a planar fit between map3 and map2
+    line_residual  = zeros(output_npix) # residual for the linear fit
+    plane_residual = zeros(output_npix) # residual for the planar fit
+
+    meta = Pipeline.Common.getmeta(4, "rainy")
+    frame = TTCal.reference_frame(meta)
+    for idx = 1:output_npix
+        θ, ϕ = LibHealpix.pix2ang_ring(output_nside, idx)
+        galactic = Direction(dir"GALACTIC", (π/2-θ)*radians, ϕ*radians)
+        itrf = measure(frame, galactic, dir"ITRF")
+        dec = latitude(itrf) |> rad2deg
         dec < -30 && continue
-        vec = LibHealpix.ang2vec(θ, ϕ)
-        disc = LibHealpix.query_disc(maps[1], θ, ϕ, deg2rad(1.5), inclusive=true)
-        distance = Float64[]
-        for jdx in disc
-            vec′ = LibHealpix.pix2vec_ring(nside(maps[1]), Int(jdx))
-            push!(distance, acosd(dot(vec, vec′)))
-        end
-        fwhm = 1 # degree
-        σ = fwhm/(2sqrt(2log(2)))
-        weights = exp(-distance.^2/(2*σ^2))
-        spectral_index[idx] = fit_spectral_index(ν, maps, disc, weights)
+
+        disc, weights = _disc_weights(map1, θ, ϕ, width)
+        m, a, b, line_res, plane_res = _fit_plane(map1, map2, map3, disc, weights)
+        line_slope[idx] = m
+        plane_slope_1[idx] = a
+        plane_slope_2[idx] = b
+        line_residual[idx] = line_res
+        plane_residual[idx] = plane_res
     end
-    spectral_index
+    line_slope, plane_slope_1, plane_slope_2, line_residual, plane_residual
 end
 
-function fit_spectral_index(ν, maps, disc, weights)
-    e = ones(length(disc))
-    slopes = zeros(length(disc)-1)
-    errors = zeros(length(disc)-1)
-    for idx = 1:length(maps)-1
-        ν1 = ν[idx]
-        ν2 = ν[idx+1]
-        map1 = maps[idx]
-        map2 = maps[idx+1]
-
-        x = [map1[pixel] for pixel in disc]
-        y = [map2[pixel] for pixel in disc]
-        A = [x e]
-        W = Diagonal(weights)
-        M = (A'*W*A)\(A'*W)
-        m = M*y
-        Σ = M*Diagonal(e)*M'
-
-        slopes[idx] = m[1]
-        errors[idx] = sqrt(Σ[1, 1]) # needs to be scaled by thermal noise in both maps
+function _disc_weights(map, θ, ϕ, width)
+    vec = LibHealpix.ang2vec(θ, ϕ)
+    disc = LibHealpix.query_disc(map, θ, ϕ, deg2rad(width), inclusive=false)
+    distance = Float64[]
+    for jdx in disc
+        vec′ = LibHealpix.pix2vec_ring(nside(map), Int(jdx))
+        push!(distance, acosd(clamp(dot(vec, vec′), -1, 1)))
     end
-    slopes, errors
+    # we want a weighting function whose value and first derivative goes to exactly zero at
+    # the boundary
+    distance = clamp(distance, 0, width)
+    weights = (cos(π*distance/width)+1)/2
+    disc, weights
+end
+
+using PyPlot
+
+function _fit_plane(map1, map2, map3, disc, weights)
+    e = ones(length(disc))
+    x = [map1[pixel] for pixel in disc]
+    y = [map2[pixel] for pixel in disc]
+    z = [map3[pixel] for pixel in disc]
+    W = Diagonal(weights)
+
+    # Fit a line
+    A = [x e]
+    m_line = (A'*W*A)\(A'*(W*z))
+    z_ = m_line[1]*x + m_line[2]
+    residual_line = sqrt(sum(abs2(z-z_)))
+
+    # Fit a plane
+    A = [x y e]
+    m_plane = (A'*W*A)\(A'*(W*z))
+    z_ = m_plane[1]*x + m_plane[2]*y + m_plane[3]
+    residual_plane = sqrt(sum(abs2(z-z_)))
+
+    #figure(1); clf()
+    #scatter(x, y, c=z, s=weights, vmin=minimum(z), vmax=maximum(z))
+    #colorbar()
+    #xlim(minimum(x), maximum(x))
+    #ylim(minimum(y), maximum(y))
+    #@show m_line m_plane
+    #@show residual_line residual_plane
+
+    m_line[1], m_plane[1], m_plane[2], residual_line, residual_plane
 end
 
 #function compare_with_haslam()
@@ -291,6 +360,20 @@ function smooth(map, width, output_nside=nside(map))
     end
 
     alm2map(output_alm, output_nside)
+end
+
+"degrade the map to a lower nside so that adjacent pixels aren't correlated anymore"
+function degrade(map, new_nside)
+    new_npix = nside2npix(new_nside)
+    output = zeros(new_npix)
+    normalization = zeros(Int, new_npix)
+    for idx = 1:length(map)
+        vec = LibHealpix.pix2vec_ring(nside(map), idx)
+        jdx = LibHealpix.vec2pix_ring(new_nside, vec)
+        output[jdx] += map[idx]
+        normalization[jdx] += 1
+    end
+    HealpixMap(output ./ normalization)
 end
 
 end
