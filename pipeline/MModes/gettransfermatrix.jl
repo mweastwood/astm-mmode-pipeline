a20bb611b39fe679aeda4de6b8e25efaf37cc281
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-11-20 11:54:36 -0800

    clean out transfer matrix generation cruft

--- pipeline/MModes/gettransfermatrix.jl
+++ pipeline/MModes/gettransfermatrix.jl
@@ -1,8 +1,8 @@
-using PyPlot
-
 function gettransfermatrix(spw, dataset)
     dir = getdir(spw)
     ttcal_metadata = getmeta(spw, dataset)
+    frame = TTCal.reference_frame(ttcal_metadata)
+    ttcal_metadata.phase_center = measure(frame, ttcal_metadata.phase_center, dir"ITRF")
     bpjspec_metadata = BPJSpec.from_ttcal(ttcal_metadata)
 
     coeff = load("/lustre/mweastwood/mmode-analysis/workspace/spw18/beam.jld", "I-coeff")
@@ -11,88 +11,9 @@ function gettransfermatrix(spw, dataset)
         beam(coeff, threshold, azimuth, elevation)
     end
 
-    lmax, divisions, categories, cumulative_space = BPJSpec.baseline_hierarchy(bpjspec_metadata)
-
-    figure(3); clf()
-    plot(cumulative_space, "k.")
-
-    total = 0.0
-    for idx = 1:length(divisions)-1
-        lmax_ = divisions[idx+1]
-        Nbase = sum(categories .== idx)
-        space = Nbase*lmax_*lmax_*128/(8*1024^3)
-        total += space
-        @show lmax_, space
-    end
-    @show total
-
-    figure(1); clf()
-    for idx = 1:length(divisions-1)
-        plot(lmax[categories .== idx], ".")
-    end
-    for division in divisions
-        axhline(division)
-    end
-
-    figure(2); clf()
-    plt[:hist](lmax, bins=100)
-    for division in divisions
-        axvline(division, color="k")
-    end
-    xlabel("lmax")
-    ylabel("number of baselines")
-
-    #@time map = BPJSpec.create_beam_map(beam_model, bpjspec_metadata, (2001, 4001))
-
-    #figure(1); clf()
-    #imshow(map)
-    #colorbar()
-
-    #frame = TTCal.reference_frame(ttcal_metadata)
-    #β = round(Int, middle(1:Nfreq(ttcal_metadata)))
-    #ν = ttcal_metadata.channels[β:β]*u"Hz"
-
-
-    #@time beam = create_beam_map(ttcal_metadata, coeff)
-    #@time bpjspec_metadata = BPJSpec.from_ttcal(frame, ttcal_metadata, lmax, mmax, ν, beam)
-
-    #@show BPJSpec.baseline_hierarchy(bpjspec_metadata)
-
-    #path = joinpath(dir, "transfermatrix-$lmax-$mmax")
-
-    #@time rhat = BPJSpec.unit_vectors(beam)
-    #@time plan = BPJSpec.plan_sht(lmax, mmax, size(beam))
-    #@time real_coeff, real_fringe = BPJSpec.fringe_pattern(bpjspec_metadata.baselines[2],#30741],
-    #                                                      bpjspec_metadata.phase_center,
-    #                                                      bpjspec_metadata.beam,
-    #                                                      rhat, plan, ν[1])
-    #return real_coeff, real_fringe, plan
-
-    #figure(1); clf()
-    ##subplot(1, 2, 1)
-    #imshow(abs.(real_coeff.matrix))
-    #gca()[:set_aspect]("auto")
-    #colorbar()
-    ##subplot(1, 2, 2)
-    ##imshow(abs.(imag_coeff.matrix))
-    ##gca()[:set_aspect]("auto")
-    ##colorbar()
-    #tight_layout()
-
-    #figure(1); clf()
-    #subplot(2, 1, 1)
-    #imshow(real_coeff)
-    #colorbar()
-    #subplot(2, 1, 2)
-    #imshow(imag_coeff)
-    #colorbar()
-
-    # TEST
-    #variables = BPJSpec.TransferMatrixVariables(meta, lmax, mmax, nside)
-    #α = 30741
-    #return @time BPJSpec.fringes(beam, variables, meta.channels[1], α)
-    # TEST
-    #transfermatrix = TransferMatrix(path, meta, beam, lmax, mmax, nside)
+    path = joinpath(dir, "transfermatrix")
+    transfermatrix = BPJSpec.HierarchicalTransferMatrix(path, bpjspec_metadata, beam_model)
+    BPJSpec.compute!(transfermatrix)
 end
 
 function beam(coeff, threshold, azimuth, elevation)
