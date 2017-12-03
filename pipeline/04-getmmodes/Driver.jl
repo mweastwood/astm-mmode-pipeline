c2ea00cd4b919d203510e2a6dde5737555fc4c41
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-12-02 16:11:48 -0800

    getmmodes

--- pipeline/04-getmmodes/Driver.jl
+++ pipeline/04-getmmodes/Driver.jl
@@ -9,21 +9,25 @@ include("../lib/Common.jl"); using .Common
 
 function getmmodes(spw, name)
     path =  getdir(spw, name)
-    meta = getmeta(spw, name)
+    mmax = (6628-1) ÷ 2
     jldopen(joinpath(path, "folded-visibilities.jld2"), "r") do input_file
         ttcal_metadata   = input_file["metadata"]
         bpjspec_metadata = BPJSpec.from_ttcal(ttcal_metadata)
-
-
-
-        prg = Progress(length(frequencies))
-        for index = 1:length(frequencies)
-            getmmodes(spw, name, input_file[@sprintf("%06d", index)], meta, frequencies, times)
+        mmodes = MModes(joinpath(path, "m-modes"), bpjspec_metadata, mmax)
+        prg = Progress(Nfreq(ttcal_metadata))
+        for index = 1:Nfreq(ttcal_metadata)
+            _getmmodes(mmodes, input_file[@sprintf("%06d", index)], index)
             next!(prg)
         end
     end
 end
 
+function _getmmodes(mmodes, array, index)
+    # put time on the fast axis
+    transposed_array = permutedims(array, (2, 1))
+    # compute the m-modes
+    BPJSpec.compute!(mmodes, transposed_array, mmodes.metadata.frequencies[index])
+end
 
 end
 
