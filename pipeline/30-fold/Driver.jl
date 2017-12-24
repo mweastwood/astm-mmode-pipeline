57c2fe8c3486abf22c615b362f48de7ec5fa83e2
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-12-24 15:48:22 -0800

    fix bug in fold (it was doing way too much work)

--- pipeline/30-fold/Driver.jl
+++ pipeline/30-fold/Driver.jl
@@ -6,39 +6,46 @@ using TTCal
 
 include("../lib/Common.jl"); using .Common
 
+const DAY = 6628
+
 function fold(spw, name)
     path =  getdir(spw, name)
     jldopen(joinpath(path, "peeled-visibilities.jld2"), "r") do input_file
         metadata = input_file["metadata"]
         jldopen(joinpath(path, "folded-visibilities.jld2"), "w") do output_file
             for frequency = 1:Nfreq(metadata)
-                fold(input_file, output_file, frequency)
+                fold(input_file, output_file, metadata, frequency)
             end
             output_file["metadata"] = metadata
         end
     end
 end
 
-function fold(input_file, output_file, frequency)
-    input_data  = input_file[o6d(frequency)]
-    output_data = zeros(Complex128, size(input_data, 2), 6628)
-    pack!(output_data, input_data)
-    output_file[o6d(frequency)] = output_data
-end
-
-function pack!(output, input)
-    _, Nbase, Ntime = size(input)
-    Ntime′  = size(output, 2)
-    weights = zeros(Int, size(output))
-    for i = 1:Ntime
-        i′ = mod1(i, Ntime′)
-        for α = 1:Nbase
-            output[α, i′]  += input[1, α, i]
-            output[α, i′]  += input[2, α, i]
-            weights[α, i′] += 1
-        end
+function fold(input_file, output_file, metadata, frequency)
+    output  = zeros(Complex128, Nbase(metadata), DAY)
+    weights = zeros(       Int, Nbase(metadata), DAY)
+    prg = Progress(Ntime(metadata))
+    for time = 1:Ntime(metadata)
+        data = input_file[o6d(time)]
+        pack!(output, weights, data, frequency, time)
+        next!(prg)
     end
     output ./= weights
+    output_file[o6d(frequency)] = output
+end
+
+function pack!(output, weights, data, frequency, time)
+    i = mod1(time, DAY)
+    Nbase = size(output, 1)
+    for α = 1:Nbase
+        xx = data[1, frequency, α]
+        yy = data[2, frequency, α]
+        if xx != 0 && yy != 0
+            output[α, i]  += xx
+            output[α, i]  += yy
+            weights[α, i] += 1
+        end
+    end
 end
 
 end
