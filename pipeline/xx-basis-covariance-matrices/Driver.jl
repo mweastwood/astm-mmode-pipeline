f80539fedf223a36d4bfb9a358a83493eb3578a0
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-02-01 17:58:45 -0800

    minor fixes and changes to basis covariance matrices

--- pipeline/xx-basis-covariance-matrices/Driver.jl
+++ pipeline/xx-basis-covariance-matrices/Driver.jl
@@ -9,7 +9,7 @@ include("../lib/Common.jl"); using .Common
 function covariance(spw, name)
     path  = getdir(spw, name)
     path′ = joinpath(path, "basis-covariance-matrices")
-    isdir(path′) && mkdir(path′)
+    isdir(path′) || mkdir(path′)
     model = fiducial()
 
     file = joinpath(path, "transfer-matrix-averaged")
@@ -18,19 +18,19 @@ function covariance(spw, name)
     frequencies = transfermatrix.frequencies
 
     for j = 1:length(model.kperp), i = 1:length(model.kpara)
-        model.power[i, j] = 1.0u"K^2*Mpc^3"
+        model.power[i, j] = 1e10u"K^2*Mpc^3"
         file = joinpath(path′, @sprintf("%03d-%03d", i, j))
         matrix = BPJSpec.AngularCovarianceMatrix(file, lmax, frequencies, model)
         BPJSpec.compute!(matrix)
         model.power[i, j] = 0.0u"K^2*Mpc^3"
     end
 
-    save(joinpath(path, "FIDUCIAL.jld2"), "model", model)
+    save(joinpath(path′, "FIDUCIAL.jld2"), "model", model)
 end
 
 function fiducial()
     # logarithmic bins (but include 0 Mpc⁻¹)
-    kpara = logspace(-4, +1, 10).*u"Mpc^-1"
+    kpara = logspace(-4, +0, 10).*u"Mpc^-1"
     kperp = logspace(-4, -1, 11).*u"Mpc^-1"
     unshift!(kpara, 0u"Mpc^-1")
     unshift!(kperp, 0u"Mpc^-1")
