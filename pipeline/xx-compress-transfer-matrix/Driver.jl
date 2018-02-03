f623f7ba76143725575108763c767ee51b71c241
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-02-02 19:37:07 -0800

    average more channels together, because I think we can get away with it

--- pipeline/xx-compress-transfer-matrix/Driver.jl
+++ pipeline/xx-compress-transfer-matrix/Driver.jl
@@ -13,8 +13,11 @@ function compress(spw, name)
     file = joinpath(path, "transfer-matrix-compressed")
     transfermatrix′ = BPJSpec.lossless_compress(file, transfermatrix)
 
+    # According to this calculation, averaging 10 channels should limit us to k < 1.0 Mpc^-1, which is fine
+    # julia> 2π/(BPJSpec.comoving_distance(BPJSpec.redshift(70.000u"MHz")) - BPJSpec.comoving_distance(BPJSpec.redshift(70.240u"MHz")))
+    # 1.0273400427793544 Mpc^-1
     file = joinpath(path, "transfer-matrix-averaged")
-    transfermatrix″ = BPJSpec.average_channels(file, transfermatrix′, 4)
+    transfermatrix″ = BPJSpec.average_channels(file, transfermatrix′, 10)
 end
 
 end
