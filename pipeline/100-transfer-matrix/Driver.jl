e03ac708c4de5437c3e732e637fd9303dedb4caa
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-02-27 15:16:38 -0800

    generate transfer matrix for hexagonal grid of antennas

--- pipeline/100-transfer-matrix/Driver.jl
+++ pipeline/100-transfer-matrix/Driver.jl
@@ -7,11 +7,16 @@ using TTCal
 
 include("../lib/Common.jl"); using .Common
 
-function transfermatrix(spw, name)
+function transfermatrix(spw, name; simulation="")
     path = getdir(spw, name)
     ttcal_metadata = load(joinpath(path, "raw-visibilities.jld2"), "metadata")
     frame = ReferenceFrame(ttcal_metadata)
     ttcal_metadata.phase_centers[1] = measure(frame, ttcal_metadata.phase_centers[1], dir"ITRF")
+    if simulation != ""
+        positions = simulate(simulation, ttcal_metadata)
+        resize!(ttcal_metadata.positions, length(positions))
+        ttcal_metadata.positions[:] = positions
+    end
     bpjspec_metadata = BPJSpec.from_ttcal(ttcal_metadata)
 
     coeff = load("/lustre/mweastwood/mmode-analysis/workspace/spw18/beam.jld", "I-coeff")
@@ -20,9 +25,13 @@ function transfermatrix(spw, name)
         beam(coeff, threshold, azimuth, elevation)
     end
 
-    path = joinpath(path, "transfer-matrix")
-    transfermatrix = BPJSpec.HierarchicalTransferMatrix(path, bpjspec_metadata)
-    BPJSpec.compute!(transfermatrix, beam_model, lmax=200)
+    if simulation != ""
+        path = joinpath(path, "transfer-matrix-"*simulation)
+    else
+        path = joinpath(path, "transfer-matrix")
+    end
+    transfermatrix = BPJSpec.HierarchicalTransferMatrix(path, bpjspec_metadata, lmax=200)
+    BPJSpec.compute!(transfermatrix, beam_model)
 end
 
 function beam(coeff, threshold, azimuth, elevation)
@@ -45,6 +54,40 @@ function beam(coeff, threshold, azimuth, elevation)
         amplitude *= shape
     end
     amplitude
+end
+
+function simulate(name, metadata)
+    position = mean(metadata.positions)
+    up = Direction(position)
+    north = Direction(dir"ITRF", 0, 0, 1)
+    north = Direction(north - dot(up, north)*up)
+    east  = Direction(cross(north, up))
+    if name == "hex"
+        x, y = hex(9, 12) # 217 antennas with 12 m spacing
+        positions = [Position(pos"ITRF", position.x + x[i]*east.x + y[i]*north.x,
+                                         position.y + x[i]*east.y + y[i]*north.y,
+                                         position.z + x[i]*east.z + y[i]*north.z)
+                     for i = 1:length(x)]
+    end
+    positions
+end
+
+"Create a hex of antenna positions for a simulated redundant configuration."
+function hex(n, spacing)
+    N = 2*n - 1
+    x = Float64[]
+    y = Float64[]
+    for row = 1:n
+        M = N - row + 1 # number of antennas in this row
+        x′ = collect(1:M) .* spacing
+        x  = [x; x′ - mean(x′)]
+        y  = [y; fill(√3*(row-1)/2*spacing, M)]
+        if row > 1
+            x  = [x; x′ - mean(x′)]
+            y  = [y; fill(-√3*(row-1)/2*spacing, M)]
+        end
+    end
+    x, y
 end
 
 end
