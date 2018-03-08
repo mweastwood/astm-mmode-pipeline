84c9c33effb53f9a9a3183159a7f56e3a40eab7c
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-03-08 13:07:59 -0800

    put more of the launch script in a function
    this seems to fix a seg fault

--- bin/launch.jl
+++ bin/launch.jl
@@ -22,30 +22,33 @@ function parse_commandline()
     end
     return parse_args(s)
 end
-args = parse_commandline()
 
+args = parse_commandline()
 path = abspath(normpath(args["driver"]))
 include(path)
 
-if args["local-workers"] !== nothing
-    info("Launching local workers")
-    addprocs(args["local-workers"], exeflags=`-L $path`)
-end
-
-if args["remote-workers"] !== nothing
-    info("Launching remote workers")
-    N = args["remote-workers"]
-    machines = ["astm04", "astm05", "astm06", "astm07", "astm08",
-                "astm09", "astm10", "astm11", "astm12", "astm13"]
-    addprocs([(machine, N) for machine in machines], exeflags=`-L $path`)
-end
-
-if args["local-workers"] !== nothing || args["remote-workers"] !== nothing
-    @sync for worker in workers()
-        @async remotecall_wait(include, worker, path)
+function main(args)
+    if args["local-workers"] !== nothing
+        info("Launching local workers")
+        addprocs(args["local-workers"], exeflags=`-L $path`)
     end
-end
 
-info("Starting computation")
-Driver.go(args["args"]...)
+    if args["remote-workers"] !== nothing
+        info("Launching remote workers")
+        N = args["remote-workers"]
+        machines = ["astm04", "astm05", "astm06", "astm07", "astm08",
+                    "astm09", "astm10", "astm11", "astm12", "astm13"]
+        addprocs([(machine, N) for machine in machines], exeflags=`-L $path`)
+    end
+
+    if args["local-workers"] !== nothing || args["remote-workers"] !== nothing
+        @sync for worker in workers()
+            @async remotecall_wait(include, worker, path)
+        end
+    end
+
+    info("Starting computation")
+    Driver.go(args["args"]...)
+end
+main(args)
 
