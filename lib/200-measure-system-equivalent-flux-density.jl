05631e61b18ea152b3f21c12043b4caa2e530882
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-04-25 10:45:49 -0700

    fix sefd calculation

--- lib/200-measure-system-equivalent-flux-density.jl
+++ lib/200-measure-system-equivalent-flux-density.jl
@@ -12,7 +12,6 @@ using PyCall
 @pyimport matplotlib.pyplot as pyplot
 
 include("Project.jl")
-include("WSClean.jl")
 include("TTCalDatasets.jl")
 using .TTCalDatasets
 
@@ -31,18 +30,20 @@ function load(file)
            dict["integration"])
 end
 
-function go(project_file, wsclean_file, config_file)
+function go(project_file, config_file)
     project = Project.load(project_file)
-    wsclean = WSClean.load(wsclean_file)
     config  = load(config_file)
-    sefd(project, wsclean, config)
+    sefd(project, config)
 end
 
-function sefd(project, wsclean, config)
+function sefd(project, config)
     path = Project.workspace(project)
     input = BPJSpec.load(joinpath(path, config.input))
     metadata = Project.load(project, config.metadata, "metadata")
     ν = ustrip.(uconvert.(u"MHz", metadata.frequencies))
+
+    Ω = 2.4u"sr"
+    K = (u"c"/(ν[1]*u"MHz"))^2/(Ω*2u"k")
 
     # Mozdzen et al. 2017 report a measurement of the absolute sky flux in the southern hemisphere
     # between 90 and 190 MHz. The brightness temperature at 150 MHz varies between 842 K at 17 h
@@ -51,96 +52,80 @@ function sefd(project, wsclean, config)
     # absorption).
     high_temp = 842u"K" * (73/150)^-2.5
     low_temp  = 257u"K" * (73/150)^-2.6
-    high_prediction = uconvert(u"Jy", 2u"k"*high_temp*(2.41u"sr")*(73u"MHz")^2/(u"c^2"))
-    low_prediction  = uconvert(u"Jy", 2u"k"* low_temp*(2.41u"sr")*(73u"MHz")^2/(u"c^2"))
+    @show low_temp high_temp
+    high_prediction = uconvert(u"Jy", high_temp/K)
+    low_prediction  = uconvert(u"Jy", low_temp/K)
     lwa1_prediction = 256*4680u"Jy"
+    lwa1_temp = 1740u"K"
 
-    #V1 = input[config.integration-1]
-    #V2 = input[config.integration+0]
-    #V3 = input[config.integration+1]
-    #differences(V1, V2, V3)
+    kelvin(x) = ustrip(uconvert(u"K",  x))
+    jansky(x) = ustrip(uconvert(u"Jy", x))
 
-    figure(1); clf()
-    fill_between(ν,
-                 fill(ustrip( low_prediction)/1e6, length(ν)),
-                 fill(ustrip(high_prediction)/1e6, length(ν)),
-                 facecolor="black", alpha=0.25)
-    axhline(ustrip(lwa1_prediction)/1e6, color="k", linestyle="-.", linewidth=1)
-    axhline(ustrip(high_prediction)/1e6, color="k", linestyle="--", linewidth=1)
-    axhline(ustrip( low_prediction)/1e6, color="k", linestyle="--", linewidth=1)
+    sefds = Vector{typeof(1.0u"Jy")}[]
     for integration = 10:500:6628
         V1 = input[integration-1]
         V2 = input[integration+0]
         V3 = input[integration+1]
-        sefd = differences(V1, V2, V3)./1e6
-        plot(ν[2:end-1], sefd, "k-", linewidth=1)
+        sefd = measure(V1, V2, V3)
+        push!(sefds, sefd)
     end
 
+    figure(1); clf()
+    fill_between(ν,
+                 fill(jansky( low_prediction)/1e6, length(ν)),
+                 fill(jansky(high_prediction)/1e6, length(ν)),
+                 facecolor="black", alpha=0.25)
+    for sefd in sefds
+        plot(ν[2:end-1], jansky.(sefd)/1e6, "k-", linewidth=1)
+    end
+    axhline(jansky(lwa1_prediction)/1e6, color="r", linestyle="-.", linewidth=1)
+    axhline(jansky(high_prediction)/1e6, color="k", linestyle="--", linewidth=1)
+    axhline(jansky( low_prediction)/1e6, color="k", linestyle="--", linewidth=1)
     xlabel("Frequency / MHz")
     ylabel("SEFD / MJy")
     xlim(ν[1], ν[end])
+
+    figure(2); clf()
+    fill_between(ν,
+                 fill(kelvin( low_temp), length(ν)),
+                 fill(kelvin(high_temp), length(ν)),
+                 facecolor="black", alpha=0.25)
+    for sefd in sefds
+        plot(ν[2:end-1], kelvin.(K*sefd), "k-", linewidth=1)
+    end
+    axhline(kelvin(lwa1_temp), color="r", linestyle="-.", linewidth=1)
+    axhline(kelvin(high_temp), color="k", linestyle="--", linewidth=1)
+    axhline(kelvin( low_temp), color="k", linestyle="--", linewidth=1)
+    xlabel("Frequency / MHz")
+    ylabel("System Temperature / K")
+    xlim(ν[1], ν[end])
+
+    nothing
 end
 
 rms(X) = sqrt(mean(abs2.(X)))
-rms(X, N) = sqrt.(mean(abs2.(X), N))
+rms(X, N) = squeeze(sqrt.(mean(abs2.(X), N)), N)
 mad(X) = median(abs.(X))
-mad(X, N) = median(abs.(X), N)
-
-difference_time(V1, V2, V3) = @. V1 - (V2 + V3)/2
-difference_freq(V) = V[:, 2:end-1, :] - (V[:, 1:end-2, :] + V[:, 3:end, :])/2
-difference_pol(V) = @. (V[1, :, :] - V[2, :, :])/2
+mad(X, N) = squeeze(median(abs.(X), N), N)
 
 function difference_all(V1, V2, V3)
     _, Nfreq, Nbase = size(V1)
     output = zeros(Complex128, Nfreq-2, Nbase)
     for α = 1:Nbase, β = 2:Nfreq-1
-        xx = V2[1, β, α] - (V1[1, β, α] + V3[1, β, α] + V2[1, β-1, α] + V2[1, β+1, α])/4
-        yy = V2[2, β, α] - (V1[2, β, α] + V3[2, β, α] + V2[2, β-1, α] + V2[2, β+1, α])/4
-        output[β-1, α] = (xx - yy)/2
+        xx = 4V2[1, β, α] - (V1[1, β, α] + V3[1, β, α] + V2[1, β-1, α] + V2[1, β+1, α])
+        yy = 4V2[2, β, α] - (V1[2, β, α] + V3[2, β, α] + V2[2, β-1, α] + V2[2, β+1, α])
+        output[β-1, α] = (xx - yy) / 2
     end
-    output
+    output ./ sqrt(10)
 end
 
-function differences(V1, V2, V3)
+function measure(V1, V2, V3)
     f = squeeze(all(V1 .== 0, (1, 2)), (1, 2)) # flags
-    N = uconvert(NoUnits, 2*24u"kHz"*13u"s")
-
-    #Δt    = difference_time(V1, V2, V3)
-    #Δf1   = difference_freq(V1)
-    #Δf2   = difference_freq(V1)
-    #Δf3   = difference_freq(V1)
-    #Δpol1 = difference_pol(V1)
-    #Δpol2 = difference_pol(V2)
-    #Δpol3 = difference_pol(V3)
-    Δall  = difference_all(V1, V2, V3) .* sqrt(N) ./ sqrt(5/8)
-
-    #figure(1); clf()
-    #semilogy(1:107, rms(   V2[1, :, .!f], 2).*sqrt(N), "k-")
-    #semilogy(1:107, rms(   Δt[1, :, .!f], 2).*sqrt(N)./sqrt(3), "r-")
-    #semilogy(2:106, rms(  Δf2[1, :, .!f], 2).*sqrt(N)./sqrt(3), "g-")
-    #semilogy(1:107, rms(Δpol2[   :, .!f], 2).*sqrt(N), "b-")
-    #semilogy(2:106, rms( Δall[   :, .!f], 2), "k-")
-    #semilogy(2:106, mad( Δall[   :, .!f], 2), "r-")
-
-    #for scale = 1.0:0.1:2.0
-    #    axhline(ustrip(scale.*prediction), color="k", linestyle="--")
-    #end
-
-    #figure(1); clf()
-    #samples = vec(abs.(Δall[:, .!f]))
-    #pyplot.hist(samples, bins=linspace(0, 10e6), histtype="step", label="data")
-    #tight_layout()
-
-    #@show rms(samples)
-    #@show mad(samples)
-
-    #test = abs.(complex.(randn(length(samples)), randn(length(samples)))) * rms(samples) / √2
-    #pyplot.hist(test, bins=linspace(0, 10e6), histtype="step", label="rms")
-    #test = abs.(complex.(randn(length(samples)), randn(length(samples)))) * mad(samples) / √2
-    #pyplot.hist(test, bins=linspace(0, 10e6), histtype="step", label="mad")
-    #legend()
-
-    rms(Δall[:, .!f], 2)
+    # for just the real or imaginary component of the visibilities, you get an additional factor of
+    # two here (ie. N = 2×Δν×τ)
+    N = uconvert(NoUnits, 24u"kHz"*13u"s")
+    Δall  = difference_all(V1, V2, V3) .* sqrt(N)
+    rms(Δall[:, .!f], 2) .* u"Jy"
 end
 
 end
