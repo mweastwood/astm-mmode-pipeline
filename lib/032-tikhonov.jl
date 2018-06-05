ea8e8aabf093963b0d849f9a8d9f647d6d69e490
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-06-04 17:51:45 -0700

    rotate the sky maps to galactic coordinates
    (as opposed to the ITRF coordinates they were in before... literally anything is better than that)

--- lib/032-tikhonov.jl
+++ lib/032-tikhonov.jl
@@ -1,9 +1,11 @@
 module Driver
 
 using BPJSpec
+using CasaCore.Measures
 using JLD2
 using LibHealpix
 using ProgressMeter
+using Unitful, UnitfulAstro
 using YAML
 
 include("Project.jl")
@@ -13,6 +15,7 @@ struct Config
     output_alm :: String
     output_map :: String
     output_directory :: String
+    metadata :: String
     transfermatrix :: String
     regularization :: Float64
     nside :: Int
@@ -25,6 +28,7 @@ function load(file)
            dict["output-alm"],
            dict["output-map"],
            get(dict, "output-directory", ""),
+           dict["metadata"],
            dict["transfer-matrix"],
            dict["regularization"],
            dict["nside"],
@@ -42,6 +46,8 @@ function tikhonov(project, config)
     path = Project.workspace(project)
     mmodes = BPJSpec.load(joinpath(path, config.input))
     transfermatrix = BPJSpec.load(joinpath(path, config.transfermatrix))
+    metadata = Project.load(project, config.metadata, "metadata")
+
     if config.mfs
         alm = BPJSpec.tikhonov(transfermatrix, mmodes, mfs=config.mfs,
                                regularization=config.regularization)
@@ -54,8 +60,10 @@ function tikhonov(project, config)
     end
 
     if config.mfs
-        map = create_map(alm, config.nside)
-        writehealpix(joinpath(path, config.output_map*".fits"), map, replace=true)
+        map = create_map(alm, metadata, config.nside)
+        map = rotate_to_galactic(map, metadata)
+        writehealpix(joinpath(path, config.output_map*".fits"), map,
+                     coordsys="G", replace=true)
     else
         Nfreq = length(alm.frequencies)
         if !isdir(joinpath(path, config.output_directory))
@@ -63,15 +71,17 @@ function tikhonov(project, config)
         end
         prg = Progress(Nfreq)
         for β = 1:Nfreq
-            map = create_map(alm, config.nside, β)
+            map = create_map(alm, metadata, config.nside, β)
+            map = rotate_to_galactic(map, metadata)
             filename = @sprintf("%s-%04d.fits", config.output_map, β)
-            writehealpix(joinpath(path, config.output_directory, filename), map, replace=true)
+            writehealpix(joinpath(path, config.output_directory, filename), map,
+                         coordsys="G", replace=true)
             next!(prg)
         end
     end
 end
 
-function create_map(alm::MBlockVector, nside)
+function create_map(alm::MBlockVector, metadata, nside)
     lmax = mmax = alm.mmax
     _alm = Alm(Complex128, lmax, mmax)
     for m = 1:lmax
@@ -83,7 +93,7 @@ function create_map(alm::MBlockVector, nside)
     alm2map(_alm, nside)
 end
 
-function create_map(alm::MFBlockVector, nside, β)
+function create_map(alm::MFBlockVector, metadata, nside, β)
     lmax = mmax = alm.mmax
     _alm = Alm(Complex128, lmax, mmax)
     for m = 1:lmax
@@ -93,6 +103,27 @@ function create_map(alm::MFBlockVector, nside, β)
         end
     end
     alm2map(_alm, nside)
+end
+
+function rotate_to_galactic(map, metadata)
+    x = Direction(dir"ITRF", 1, 0, 0)
+    y = Direction(dir"ITRF", 0, 1, 0)
+    z = Direction(dir"ITRF", 0, 0, 1)
+
+    frame = ReferenceFrame(metadata)
+    ξ = measure(frame, x, dir"GALACTIC")
+    η = measure(frame, y, dir"GALACTIC")
+    ζ = measure(frame, z, dir"GALACTIC")
+
+    output = RingHealpixMap(eltype(map), map.nside)
+    for idx = 1:length(map)
+        vec = LibHealpix.pix2vec(map, idx)
+        dir = Direction(dir"GALACTIC", vec.x, vec.y, vec.z)
+        θ =  acos(dot(dir, ζ))
+        ϕ = atan2(dot(dir, η), dot(dir, ξ))
+        output[idx] = LibHealpix.interpolate(map, θ, ϕ)
+    end
+    output
 end
 
 end
