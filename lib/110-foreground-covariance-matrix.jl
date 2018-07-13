a9323fcff92a139a75c93c5e7cf67752a5b9cbd9
Michael Eastwood <mweastwood@astro.caltech.edu>
2018-07-12 20:28:56 -0700

    make the foreground covariance matrix correct

--- lib/110-foreground-covariance-matrix.jl
+++ lib/110-foreground-covariance-matrix.jl
@@ -2,17 +2,29 @@ module Driver
 
 using BPJSpec
 using YAML
+using Unitful
 
 include("Project.jl")
 
 struct Config
     input  :: String
     output :: String
+    ν0 :: Float64
+    A  :: Float64
+    α  :: Float64
+    β  :: Float64
+    ζ  :: Float64
 end
 
 function load(file)
     dict = YAML.load(open(file))
-    Config(dict["input"], dict["output"])
+    Config(dict["input"],
+           dict["output"],
+           get(dict, "nu", 0.0),
+           get(dict, "A",  0.0),
+           get(dict, "alpha", 0.0),
+           get(dict, "beta",  0.0),
+           get(dict, "zeta",  0.0))
 end
 
 function go(project_file, config_file)
@@ -27,14 +39,22 @@ function foreground_covariance(project, config)
     mmodes = BPJSpec.load(joinpath(path, config.input))
     lmax = mmodes.mmax
 
-    points   = create(AngularCovarianceMatrix, NoFile(), BPJSpec.extragalactic_point_sources(),
-                      lmax, mmodes.frequencies, mmodes.bandwidth, progress=true)
-    galactic = create(AngularCovarianceMatrix, NoFile(), BPJSpec.galactic_synchrotron(),
-                      lmax, mmodes.frequencies, mmodes.bandwidth, progress=true)
-
-    foregrounds = create(LBlockMatrix, SingleFile(joinpath(path, config.output)),
-                         lmax, mmodes.frequencies, mmodes.bandwidth, rm=true) |> ProgressBar
-    @. foregrounds = points + galactic
+    if config.ν0 == 0
+        points   = create(AngularCovarianceMatrix, NoFile(), BPJSpec.extragalactic_point_sources(),
+                          lmax, mmodes.frequencies, mmodes.bandwidth, progress=true)
+        galactic = create(AngularCovarianceMatrix, NoFile(), BPJSpec.galactic_synchrotron(),
+                          lmax, mmodes.frequencies, mmodes.bandwidth, progress=true)
+        foregrounds = create(LBlockMatrix, SingleFile(joinpath(path, config.output)),
+                             lmax, mmodes.frequencies, mmodes.bandwidth, rm=true) |> ProgressBar
+        @. foregrounds = points + galactic
+    else
+        foreground = BPJSpec.ForegroundComponent(config.ν0 * u"MHz",
+                                                 config.A  * u"mK^2",
+                                                 config.α, config.β, config.ζ)
+        foregrounds = create(AngularCovarianceMatrix, SingleFile(joinpath(path, config.output)),
+                             foreground, lmax, mmodes.frequencies, mmodes.bandwidth,
+                             rm=true, progress=true)
+    end
 end
 
 end
